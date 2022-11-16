4571795aba1a81ea8c4934ab8e409d0bb4606ea5
fongas01 <nando.fongas@gmail.com>
2022-11-16 18:20:26 -0300

    Funcionalidade ao Whatsapp

--- js/custom.js
+++ js/custom.js
@@ -45,12 +45,3 @@ function myMap() {
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
-/* WHATSAPP */
-function whatsapp() {
-    let mensagem;
-
-    mensagem = "Olá, meu nome é Aline." + " Como posso te ajudar" + "?";
-
-    // incluir o método de envio para whatsapp
-    window.open("https:\\wa.me/+5521981971633?text=" + mensagem);
-}
